fd603c7383d48e4f027773a9b55539268d171b94
hegde-atri <iamatrihegde@gmail.com>
2021-07-21 19:59:41 +0100

    HttpPut

---
+++ Data/CampProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Models;
+using CoreCodeCamp.Data;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
+
+            CreateMap<Talk, TalkModel>();
+            CreateMap<Speaker, SpeakerModel>();
+            CreateMap<CampModel, Camp>();
+        }
+    }
+}
